78af7737149483c723ec60099a86e6c8483dd960
Owez <root@ogriffiths.com>
2021-06-19 17:59:36 +0100

    Added token details for user_provider

--- migrations/user_provider/up.sql
+++ migrations/user_provider/up.sql
@@ -1,5 +1,8 @@
 CREATE TABLE user_provider (
     id INTEGER PRIMARY KEY,
+    token_access VARCHAR(64) NOT NULL,
+    token_refresh VARCHAR(64),
+    token_expires TIMESTAMP WITH TIME ZONE,
     provider_id SERIAL FOREIGN KEY REFERENCES provider(id) NOT NULL,
     created TIMESTAMP WITH TIME ZONE NOT NULL
 );
